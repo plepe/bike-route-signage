ce13ced2962a3594693001a36e16da27d5ab3a3e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-10-30 20:40:24 +0100

    ConfigureView: use modulekit-form

--- src/ConfigureView.js
+++ src/ConfigureView.js
@@ -1,15 +1,37 @@
+const Form = require('modulekit-form')
 const Tab = require('modulekit-tabs').Tab
 
 const updateInput = require('./updateInput')
 
 module.exports = class ConfigureView {
   constructor (app) {
-    const div = document.getElementById('configureView')
+    const div = document.createElement('form')
+    div.id = 'configureView'
+
     this.tab = new Tab({ id: 'configureView' })
     app.tabs.add(this.tab)
 
     this.tab.header.innerHTML = 'Ansicht'
     this.tab.content.appendChild(div)
+
+    this.form = new Form('view', {
+      pick: {
+        name: 'Prioritäten',
+        type: 'text',
+        default: '4,3,2,1,1'
+      }
+    })
+
+    this.form.show(div)
+    this.form.onchange = () => {
+      let data = this.form.get_data()
+      global.updateStatus(data)
+    }
+
+    let submit = document.createElement('input')
+    submit.type = 'submit'
+    submit.style.display = 'hidden'
+    div.appendChild(submit)
   }
 
   setRoute (route) {
@@ -18,7 +40,9 @@ module.exports = class ConfigureView {
 
   updateStatus (options) {
     if (options.pick) {
-      updateInput('input[name=pick]', options.pick)
+      this.form.set_data({
+        pick: options.pick
+      })
     }
   }
 }
