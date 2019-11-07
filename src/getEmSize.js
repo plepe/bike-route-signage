e7fe912ba0876530d66d2940d49bd8c3075ae3d8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-11-07 09:21:34 +0100

    getEmSize(): lint

--- src/getEmSize.js
+++ src/getEmSize.js
@@ -1,35 +1,41 @@
-var em_height_cache = {};
-function get_em_height(dom_el) {
-  var font_size = elementCurrentStyle(dom_el, "font-size");
+/* global getComputedStyle:false */
 
-  if(!(font_size in em_height_cache) || em_height_cache[font_size] === 0) {
+const cache = {}
+
+module.exports = function getEmSize (domEl) {
+  const fontSize = elementCurrentStyle(domEl, 'font-size')
+
+  if (!(fontSize in cache) || cache[fontSize] === 0) {
     // calculate height of M
-    var em=document.createElement("div");
-    em.setAttribute("style", "display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em;");
-    em.appendChild(document.createTextNode("M"));
-    dom_el.appendChild(em);
-    em_height_cache[font_size] = em.offsetHeight;
-    dom_el.removeChild(em);
-
+    const em = document.createElement('div')
+    em.setAttribute('style', 'display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em;')
+    em.appendChild(document.createTextNode('M'))
+    domEl.appendChild(em)
+    cache[fontSize] = em.offsetHeight
+    domEl.removeChild(em)
   }
 
-  return em_height_cache[font_size];
+  return cache[fontSize]
 }
 
 // from: http://stackoverflow.com/a/5265175
-function elementCurrentStyle(element, styleName){
-    if (element.currentStyle){
-        var i = 0, temp = "", changeCase = false;
-        for (i = 0; i < styleName.length; i++)
-            if (styleName[i] != '-'){
-                temp += (changeCase ? styleName[i].toUpperCase() : styleName[i]);
-                changeCase = false;
-            } else {
-                changeCase = true;
-            }
-        styleName = temp;
-        return element.currentStyle[styleName];
-    } else {
-        return getComputedStyle(element, null).getPropertyValue(styleName);
+function elementCurrentStyle (element, styleName) {
+  if (element.currentStyle) {
+    let i = 0
+    let temp = ''
+    let changeCase = false
+
+    for (i = 0; i < styleName.length; i++) {
+      if (styleName[i] !== '-') {
+        temp += (changeCase ? styleName[i].toUpperCase() : styleName[i])
+        changeCase = false
+      } else {
+        changeCase = true
+      }
     }
+    styleName = temp
+    return element.currentStyle[styleName]
+  } else {
+    return getComputedStyle(element, null).getPropertyValue(styleName)
+  }
 }
